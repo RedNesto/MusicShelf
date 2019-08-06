1009672bdea5a59e3f7b258f89bc2c69592d3d5c
RedNesto <rednestomc@gmail.com>
2019-08-06 09:38:31 +0200

    Make sure file exists before opening or showing it

--- src/main/kotlin/io/github/rednesto/musicshelf/utils/DesktopHelper.kt
+++ src/main/kotlin/io/github/rednesto/musicshelf/utils/DesktopHelper.kt
@@ -1,24 +1,37 @@
 package io.github.rednesto.musicshelf.utils
 
 import java.awt.Desktop
+import java.nio.file.Files
 import java.nio.file.Path
 
 object DesktopHelper {
-    fun open(filePath: Path) = this { desktop: Desktop ->
-        desktop.open(filePath.toFile())
+    fun open(filePath: Path) {
+        if (Files.notExists(filePath)) {
+            return
+        }
+
+        this { desktop: Desktop ->
+            desktop.open(filePath.toFile())
+        }
     }
 
-    fun show(filePath: Path) = this { desktop: Desktop ->
-        val absolutePath = filePath.toAbsolutePath().toString()
-        when {
-            OsInfo.IS_WINDOWS -> Runtime.getRuntime().exec("explorer /select,\"$absolutePath\"")
-            OsInfo.IS_MACOS -> Runtime.getRuntime().exec(arrayOf("open", "-R", absolutePath))
+    fun show(filePath: Path) {
+        if (Files.notExists(filePath)) {
+            return
+        }
+
+        this { desktop: Desktop ->
+            val absolutePath = filePath.toAbsolutePath().toString()
+            when {
+                OsInfo.IS_WINDOWS -> Runtime.getRuntime().exec("explorer /select,\"$absolutePath\"")
+                OsInfo.IS_MACOS -> Runtime.getRuntime().exec(arrayOf("open", "-R", absolutePath))
+            }
         }
     }
 
     fun supportsShow(): Boolean = OsInfo.IS_WINDOWS || OsInfo.IS_MACOS
 
-    operator fun invoke(action: (desktop :Desktop) -> Unit) {
+    operator fun invoke(action: (desktop: Desktop) -> Unit) {
         if (Desktop.isDesktopSupported()) {
             action(Desktop.getDesktop())
         }

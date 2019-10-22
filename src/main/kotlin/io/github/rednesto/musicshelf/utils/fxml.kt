c5fbbe99dab358f3a9229b4340d81cab085f3ac4
RedNesto <RedNestoMC@gmail.com>
2019-10-22 13:05:02 +0200

    Load fxml files with an URL instead of an InputStream to correctly load css files declared in fxml files.

--- src/main/kotlin/io/github/rednesto/musicshelf/utils/fxml.kt
+++ src/main/kotlin/io/github/rednesto/musicshelf/utils/fxml.kt
@@ -2,16 +2,18 @@ package io.github.rednesto.musicshelf.utils
 
 import io.github.rednesto.musicshelf.MusicShelfApp
 import javafx.fxml.FXMLLoader
-import java.io.InputStream
+import java.net.URL
 import java.util.*
 
 fun <T> loadFxml(location: String, controller: Any? = null, resources: ResourceBundle? = null): T {
-    return loadFxml(MusicShelfApp::class.java.getResourceAsStream(location), controller, resources)
+    val locationUrl = MusicShelfApp::class.java.getResource(location)
+            ?: throw NullPointerException("Could not find fxml file at $location")
+    return loadFxml(locationUrl, controller, resources)
 }
 
-fun <T> loadFxml(location: InputStream, controller: Any? = null, resources: ResourceBundle? = null): T {
-    val loader = FXMLLoader()
+fun <T> loadFxml(location: URL, controller: Any? = null, resources: ResourceBundle? = null): T {
+    val loader = FXMLLoader(location)
     loader.resources = resources
     loader.setController(controller)
-    return loader.load(location)
+    return loader.load()
 }
